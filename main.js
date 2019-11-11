9e4d7121bd778a8f13998a0e916a292dd4ff6f61
paul7337 <nkosi.lawton@gmail.com>
2019-11-11 13:25:36 +0000

    Update for compatibility with electron v5+

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const {ipcMain} = require('electron')
 let win  
 
 function createWindow() { 
-   win = new BrowserWindow({width: 800, height: 600}) 
+   win = new BrowserWindow({width: 800, height: 600, webPreferences: {
+    nodeIntegration: true
+   }}) 
    win.loadURL(url.format ({ 
       pathname: path.join(__dirname, 'index.html'), 
       protocol: 'file:', 
